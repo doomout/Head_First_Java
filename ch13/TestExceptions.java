7a0d1aad99bc687112ef055f798e869d279acd27
khg <winquake3@hanmail.net>
2024-03-29 01:53:38 +0900

    13강 - 예외 처리 끝

--- ch13/TestExceptions.java
+++ ch13/TestExceptions.java
@@ -2,27 +2,43 @@ package ch13;
 
 public class TestExceptions {
     public static void main(String[] args) {
-        String test = "no";
+        String test = "yes";
         try {
-            System.out.println("start try");
+            System.out.println("try 블록 시작");
             doRisky(test);
-            System.out.println("end try");
+            System.out.println("try 블록 끝");
         } catch (ScaryException se) {
-            System.out.println("scary exception");
+            System.out.println("예외 발생");
         } finally {
-            System.out.println("finally");
+            System.out.println("finally 블록");
         }
-            System.out.println("end of main");
+            System.out.println("main 끝");
     }
 
     static void doRisky(String test) throws ScaryException {
-        System.out.println("start risky");
+        System.out.println("위험한 메서드 시작");
         if ("yes".equals(test)) {
           throw new ScaryException();
         }
-        System.out.println("end risky");
+        System.out.println("위험한 메서드 끝");
     }
 }
 
 class ScaryException extends Exception {
 }
+/**실행 결과 
+String test = "no" 일 때
+try 블록 시작
+위험한 메서드 시작
+위험한 메서드 끝
+try 블록 끝
+finally 블록
+main 끝
+
+String test = "yes" 일 때
+try 블록 시작
+위험한 메서드 시작
+예외 발생
+finally 블록
+main 끝
+ */
